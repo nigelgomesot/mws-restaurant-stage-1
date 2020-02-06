cec6d1b854e076a595602628d3c647a9909165e1
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-02-06 06:54:52 -0600

    # view details aria label added...

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', `View details about ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
