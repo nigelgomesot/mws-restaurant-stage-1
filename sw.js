201301b98c8726aa5a64486e142f3efbee471164
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-02-09 08:32:13 -0600

    # sw activate added...

--- sw.js
+++ sw.js
@@ -25,3 +25,18 @@ self.addEventListener('install', function(event) {
 		})
 	);
 });
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith(appName) &&
+						   !allCaches.includes(cacheName);
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});
