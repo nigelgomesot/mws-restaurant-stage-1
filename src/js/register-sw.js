20268cb4db3ec583ebf8412d67b062595e593813
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-03-25 09:23:26 -0500

    # register-sw WIP...

--- src/js/register-sw.js
+++ src/js/register-sw.js
@@ -1,7 +1,11 @@
-if (navigator.serviceWorker) {
-	navigator.serviceWorker.register('/sw.js').then(function(reg) {
-		console.log('SW registered successfully.');
-	}).catch(e => {
-		console.error('SW registration failed!');
-	});
+export function execute() {
+	if (navigator.serviceWorker) {
+		navigator.serviceWorker.register('/sw.js').then(function(reg) {
+			console.log('SW registered successfully.');
+		}).catch(e => {
+			console.error('SW registration failed!');
+		});
+	}
 }
+
+execute();
--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -1,6 +1,6 @@
 import * as DBHelper from './dbhelper';
 import SECRET from './secret';
-import './register-sw';
+import * as RegisterSW from './register-sw';
 
 let restaurant;
 var newMap;
