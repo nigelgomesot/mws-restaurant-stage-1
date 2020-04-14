4e9687784499d1d274211a94b0a84a9a18da0ea7
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-04-14 08:49:46 -0500

    # review form WIP...

--- src/js/review-form.js
+++ src/js/review-form.js
@@ -28,3 +28,36 @@ function clearForm() {
 	document.getElementById('rating').selectedIndex = 0;
 	document.getElementById('comments').value = '';
 }
+
+function validateAndGetData() {
+	const data = {};
+
+	const name = document.getElementById('name');
+	if (name.value === '') {
+		name.focus();
+		return;
+	}
+	data.name = name.value;
+
+	const ratingSelect = document.getElementById('rating');
+	const rating_value = ratingSelect.options[ratingSelect.selectedIndex].value;
+	if (rating_value === '--') {
+		ratingSelect.focus();
+		return;
+	}
+	data.rating = Number(rating_value);
+
+	const comments = document.getElementById('comments');
+	if (comments.value === '') {
+		comments.focus();
+		return;
+	}
+	data.comments = comments.value;
+
+	const restaurantId = document.getElementById('review-form').dataset.restaurantId;
+	data.restaurantId = Number(restaurantId);
+
+	data.createdAt = new Date().toISOString();
+
+	return data;
+}
